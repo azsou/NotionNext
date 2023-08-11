b7c24de1fcedd55857bba67bbbdfa0abcbd5ce39
AZ智能工坊 <282055881@qq.com>
2023-08-11 23:31:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,10 +17,10 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '安卓搜索', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'ai绘画,人工智能,stable diffusion🤖', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'AZ智能', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || 'ai绘画,人工智能,游戏开发🤖', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://azsou.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '人工智能,AI,搜索', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '人工智能,AI,技巧,编程,创意,搜索引擎', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
