9a4cc06bbd0d8e9eb9f725fdc3145e2bb693083c
AZ智能工坊 <282055881@qq.com>
2023-09-02 17:48:16 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -34,7 +34,7 @@ const Footer = ({ title }) => {
             <div id='footer-bottom' className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#30343f]'>
 
                 <div id='footer-bottom-left'>
-                   NotionNext {BLOG.VERSION} <i className='fas fa-copyright' /> {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart' /> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.
+                   {BLOG.AUTHOR} <i className='fas fa-copyright' /> {`${copyrightDate}`} 
                 </div>
 
             <div id='footer-bottom-right'>
