2a884aa1d385cbb8620d4528851ae259084e57c9
AZ智能工坊 <282055881@qq.com>
2025-02-19 02:15:03 -0500

    Update _document.js Add Adsense

--- pages/_document.js
+++ pages/_document.js
@@ -29,6 +29,8 @@ class MyDocument extends Document {
               />
             </>
           )}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3826640377705269"
+     crossorigin="anonymous"></script>
         </Head>
 
         <body>
